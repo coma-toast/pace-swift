ded46bf8888636c05b12e17d1d5565e84e94812b
Jason <jdale215@gmail.com>
2020-08-24 20:43:13 -0400

    design contact card a little better - learning more design stuff

--- PaCE/PaCE/ContactDetail.swift
+++ PaCE/PaCE/ContactDetail.swift
@@ -11,23 +11,34 @@ import SwiftUI
 struct ContactDetail: View {
     let contact: Contact
     var body: some View {
-        VStack{
-            ContactView.Icon(contact: contact)
-            Divider()
-            VStack(alignment: .leading) {
-                ContactView.FullName(contact: contact)
-                Text(contact.company).font(.subheadline)
-                Text(contact.email)
-                Text(contact.phone)
-                Text(contact.timezone)
+        ZStack{
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(Color.red)
+            VStack{
+                ContactView.Icon(contact: contact)
+                Divider()
+                VStack(alignment: .leading) {
+                    HStack{
+                        Text("Name: ").bold()
+                    
+                    ContactView.FullName(contact: contact)
+                    }
+                    Text(contact.company).font(.subheadline)
+                    Text(contact.email)
+                    Text(contact.phone)
+                    Text(contact.timezone)
+                
+                }
             }
         }
     }
 }
 
-//struct ContactDetail_Previews: PreviewProvider {
-//    let contact = Contact(id: "1234", created: "123", firstName: "Testy", lastName: "McContact", company: "Super Long Company Name, Co.", email: "test@superlongcompanynameco.com", phone: "1231231234", timezone: "EDT", instance: "1")
-//    static var previews: some View {
-//        ContactDetail(contact: contact)
-//    }
-//}
+struct ContactDetail_Previews: PreviewProvider {
+//    let contacts = ContactViewModel()
+    static var contact = Contact(id: "1234", created: "123", firstName: "Testy", lastName: "McContact", company: "Super Long Company Name, Co.", email: "test@superlongcompanynameco.com", phone: "1231231234", timezone: "EDT", instance: "1")
+
+    static var previews: some View {
+        ContactDetail(contact: contact)
+    }
+}
